4f4c471b2f7491634ac8dfd21ccbeac83b90c4fd
godotc <disyourself@qq.com>
2024-03-17 23:46:19 +0800

    bc

---
+++ blockchain/rs-bc/src/lib.rs
@@ -0,0 +1,160 @@
+
+
+use chrono::Utc;
+use libp2p::futures::task::waker;
+use log::{error, info, warn};
+use serde::{Serialize,Deserialize};
+
+const DIFFICULTY_PREFIX:&str = "00";
+
+fn hash_to_binary_reprsentation(hash: &[u8])->String{
+    let mut res: String = String::default();
+    for c in hash{
+        res.push_str(&format!("{:b}",c));
+    }
+    res
+}
+
+fn  mince_block(id: u64,timestamp:i64,  previous_hash:&str, data:&str) ->(u64,String){
+    info!("mining block...");
+    let mut nonce =0;
+
+    loop {
+        if nonce % 100000==0{
+            info!("nonce: {}",nonce);
+        }
+
+        let hash = calculate_hash(id, timestamp, previous_hash, data,nonce);
+        let binary_hash = hash_to_binary_reprsentation(&hash);
+        if binary_hash.starts_with(DIFFICULTY_PREFIX){
+            info!("mined! nonce:{}, hash: {}, binary hash:{}",
+                  nonce, hex::encode(&hash),binary_hash);
+        }
+    }
+}
+
+
+
+pub struct App{
+    [Copy]
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Serialize,Deserialize, Debug, Clone)]
+pub struct Block{
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp:String,
+    pub nonce: u64,
+
+    pub data:String,
+}
+
+
+impl App{
+    fn new()->Self{
+        Self{blocks:vec![]}
+    }
+
+    fn genesis(&mut self){
+        let mut genesis_block= Block{
+            id:0,
+            timestamp: Utc::now().timestamp(),
+            previous_hash: String::from("genesis"),
+            data: String::from("genesis!"),
+            nonce : 9527,
+            hash: "".into(),
+        }
+
+        //genesis_block.hash = 
+
+        self.blocks.push(genesis_block)
+    }
+
+     fn try_add_block(&mut self, block: Block) {
+        let latest_block = self.blocks.last().expect("there is at least one block");
+        if self.is_block_valid(&block, latest_block) {
+            self.blocks.push(block);
+        } else {
+            error!("could not add block - invalid");
+        }
+    }
+
+     fn choose_chain(&mut self, local:&Vec<Block> , remote:&Vec<Block>)->Vec<Block>{
+         let is_local_valid = self.is_chain_valid(&local);
+         let is_remote_valid = self.is_chain_valid(&remote);
+
+         if is_local_valid && is_remote_valid{
+             if local.len() >= remote.len(){
+                 local.clone()
+             }else{
+                 remote.clone()
+             } 
+         }else if is_remote_valid{
+             remote.clone()
+
+         }else if is_local_valid{
+             local.clone()
+         }else{
+             panic!("local and remote chain are both invalid!");
+         }
+
+     }
+
+     fn is_block_valid(&self, block:&Block, previous_block:&Block) -> bool{
+         if(block.previous_hash != previous_block.hash){
+             warn!(" block id: {}, wrong previous_hash",block.id);
+             return false;
+         }else if !hash_to_binary_reprsentation(
+             &hex::decode(&block.hash).expect("can decode from hex")
+             ).starts_with(DIFFICULTY_PREFIX){
+             warn!(" block id: {}, invalid difficultry",block.id);
+             return false;
+         }else if block.id != previous_block.id+1{
+             warn!(" block id: {}, id not equal to latest_block.id +1",block.id);
+             return false;
+         }else if hex::encode(calculate_hash(
+                 block.id,
+                 block.timestamp,
+                 &block.previous_hash,
+                 &block.data,
+                 block.nonce
+                ))!= block.hash
+         {
+             warn!(" block id: {}, invalid hash",block.id);
+             return false;
+         }
+         true
+     }
+
+     fn is_chain_valid(&self, chain:&[Block]) ->bool{
+         for i in 0..chain.len(){
+             if i==0{
+                 continue;
+             }
+             let first = chain.get(i-1).expect("has to exist");
+             let second = chain.get(i).expect("has to exist");
+             if !self.is_block_valid(second,first){
+                 return false;
+             }
+         }
+         true
+     }
+}
+
+
+impl Block{
+    pub fn new(id:u64, previous_hash:String, data:String)-> Self{
+let now = Utc::now();
+let (nonce, hash)= mince_block(id, now.timestamp(), &previous_hash, &data);
+Self{
+    id,hash,timestamp: now.timestamp(),
+    previous_hash,
+    data,
+    nonce,
+}
+    }
+
+}
+
